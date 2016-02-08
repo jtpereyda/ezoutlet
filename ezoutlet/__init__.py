a1ada989b03de79cb164ffee56bb4c07083a0a0e
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2016-02-08 14:18:49 -0800

    Remove unused import.

--- ezoutlet/__init__.py
+++ ezoutlet/__init__.py
@@ -1,8 +1,6 @@
 # Copyright (C) 2015 Schweitzer Engineering Laboratories, Inc.
 # This software may be modified and distributed under the terms
 # of the MIT license.  See the LICENSE file for details.
-import sys
-
 from .ez_outlet_reset import EzOutletReset, EzOutletResetError, EzOutletResetUsageError, main
 
 __version__ = '0.0.1-dev1'
