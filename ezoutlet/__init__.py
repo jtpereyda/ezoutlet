caaa17481644c426aa93d9667a8a4ecbef270dac
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2016-03-11 10:57:34 -0800

    Cleanup: Move error handling outu of __init__.py

--- ezoutlet/__init__.py
+++ ezoutlet/__init__.py
@@ -6,50 +6,32 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import traceback
-
-from .parse_command import parse_command
+from . import error_handling
 from . import constants
 from . import parser
 from .exceptions import EzOutletError, EzOutletUsageError
 from .ez_outlet import EzOutlet
 from .no_command import NoCommand
+from .parse_command import parse_command
 from .reset_command import ResetCommand
 
 __version__ = '0.0.1-dev3'
-
-
-def _parse_args_and_run(argv):
-    parsed_args = parser.static_parser.parse_args(argv)
-    cmd = parse_command(parsed_args.subcommand, parsed_args)
-    return cmd.run()
-
-
-def _usage_error(exception):
-    parser.print_usage()
-    parser.print_error(msg=exception)
-    return constants.EXIT_CODE_PARSER_ERR
-
-
-def _handle_error(exception):
-    parser.print_error(msg=exception)
-    return constants.EXIT_CODE_ERR
-
-
-def _handle_unexpected_error(exception):
-    _ = exception  # exception gets printed by traceback.format_exc()
-    parser.print_error(msg=constants.UNHANDLED_ERROR_MESSAGE.format(traceback.format_exc()))
-    return constants.EXIT_CODE_ERR
 
 
 def main(argv):
     try:
         return _parse_args_and_run(argv)
     except EzOutletUsageError as e:
-        return _usage_error(e)
+        return error_handling.usage_error(e)
     except EzOutletError as e:
-        return _handle_error(e)
+        return error_handling.runtime_error(e)
     except Exception as e:
-        return _handle_unexpected_error(e)
+        return error_handling.unexpected_exception(e)
     except SystemExit as e:
         return e.code
+
+
+def _parse_args_and_run(argv):
+    parsed_args = parser.static_parser.parse_args(argv)
+    cmd = parse_command(parsed_args.subcommand, parsed_args)
+    return cmd.run()
